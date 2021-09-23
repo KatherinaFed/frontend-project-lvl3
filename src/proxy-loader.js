0dbc70905b4322670fb7d6ab4d2864f681f7d98c
Katherina <fedoseeva2292@gmail.com>
2021-09-23 12:04:10 +0200

    added network Error

--- src/proxy-loader.js
+++ src/proxy-loader.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
+import { ru, en } from './locales/index.js';
+import state from './state.js';
 
 const getProxyUrl = (url) => {
   const proxyUrl = `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`;
 
   return axios.get(proxyUrl)
-    .then((response) => response.data.contents);
+    .then((response) => response.data.contents)
+    .catch(() => {
+      if (state.lang === 'ru') {
+        throw new Error(ru.translation.errors.errorNetwork);
+      }
+      throw new Error(en.translation.errors.errorNetwork);
+    });
 };
 
 export default getProxyUrl;
